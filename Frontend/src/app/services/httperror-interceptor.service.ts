480a2c4f2b0f07b69586e0994f19c6aee5af4d56
Eddy <eddyanghel910@gmail.com>
2022-10-07 16:09:48 +0300

    finished interceptor

--- Frontend/src/app/services/httperror-interceptor.service.ts
+++ Frontend/src/app/services/httperror-interceptor.service.ts
@@ -16,11 +16,26 @@ export class HttpErrorInterceptorService implements HttpInterceptor{
     return next.handle(request)
       .pipe(
           catchError((error: HttpErrorResponse) => {
+            const errorMessage = this.setError(error);
             console.log(error);
-            this.alertify.error(error.error);
-            return throwError(() => new Error('Error'));
+            this.alertify.error(errorMessage);
+            return throwError(errorMessage);
           })
       );
+  }
+
+  setError(error: HttpErrorResponse): string {
+    let errorMessage = 'Unknown error occured';
+    if (error.error instanceof ErrorEvent) {
+      //Client side error
+      errorMessage = error.error.message;
+    } else {
+      //Server side error
+      if (error.status!==0){
+      errorMessage = error.error.errorMessage;
+      }
+    }
+    return errorMessage;
   }
 
   }
